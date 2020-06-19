ca616216a63002be8998762fc283dfc55f2fe5c5
NCPlayz <chowdhurynadir0@outlook.com>
2020-06-19 02:01:34 +0100

    Some changes and push a shiny new token

--- README.md
+++ README.md
@@ -2,3 +2,33 @@
 
 A simple event logger for [discord.py](https://github.com/Rapptz/discord.py).
 
+## Install
+
+```sh
+$ python3 -m pip install -U discord-ext-science
+```
+
+## Extras
+
+### [`databases`](https://github.com/encode/databases) package
+
+You may want to manually install [`databases`](https://github.com/encode/databases) with the correct drivers installed.
+
+## Examples
+
+```py
+from science import Scientist, EventFlags, Configuration
+from science.recorders.databases import DatabasesRecorder
+import logging
+
+logging.basicConfig(filename='log.log', filemode='w', level=logging.DEBUG)
+
+config = Configuration(
+    events=EventFlags.guilds(),
+    recorder=DatabasesRecorder("url-to-my-database")
+)
+client = Scientist(config=config)
+
+
+client.run('NzE3MDM0MDE2ODM4MTIzNjYy.XuuC3A.vffXDcn6MPA_WAnZKAbxQo2wLL0')
+```
---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+version = '0.1.1'
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+# basically install databases with the correct driver if they want it.
+extras_require = {
+    'databases': 'databases',
+}
+
+setup(
+    name='discord-ext-science',
+    author='NCPlayz',
+    python_requires='>=3.7.0',
+    url='https://github.com/NCPlayz/discord-ext-science',
+    version=version,
+    packages=['discord/ext/science'],
+    license='MIT',
+    description='A simple event logger for discord.py.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require=extras_require,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ]
+)
